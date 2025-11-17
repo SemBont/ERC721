4cda3b32beaec2be2c93e42b8155d3894b0234bd
SemBont <JonathanWood1964@outlook.com>
2025-11-17 17:20:04 +0100

    Update ERC721.sol

--- ERC721.sol
+++ ERC721.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.30;
+pragma solidity ^0.8.27;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
 
